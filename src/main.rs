f538dee5afc9b7644ed9b91f668e28817332c829
Martinif <silas@gaf.fs.lmu.de>
2021-01-05 10:55:34 +0100

    Move tls to it's own file

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,17 @@ use lb_strategies::{RandomStrategy, IPHashStrategy, RoundRobinStrategy};
 use listeners::{AcceptorProducer, Https};
 use server::{BackendPool, BackendPoolConfig, SharedData};
 
-use std::io::{self, ErrorKind::InvalidData};
+use std::io;
 use std::vec;
-use std::{fs::File, io::BufReader, path::Path, sync::Arc};
+use std::{path::Path, sync::Arc};
 use tokio::try_join;
-use tokio_rustls::rustls::{
-  internal::pemfile::{certs, rsa_private_keys},
-  sign::{CertifiedKey, RSASigningKey},
-  Certificate, NoClientAuth, PrivateKey, ResolvesServerCertUsingSNI, ServerConfig,
-};
+use tokio_rustls::rustls::{NoClientAuth,  ResolvesServerCertUsingSNI, ServerConfig};
 
 mod lb_strategies;
 mod listeners;
 mod logging;
 mod server;
+mod tls;
 
 const LOCAL_HTTP_ADDRESS: &str = "0.0.0.0:80";
 const LOCAL_HTTPS_ADDRESS: &str = "0.0.0.0:443";
@@ -91,7 +88,7 @@ async fn listen_for_https_request(shared_data: Arc<SharedData>) -> Result<(), io
       BackendPoolConfig::HttpsConfig {
         certificate_path,
         private_key_path,
-      } => add_certificate(
+      } => tls::add_certificate(
         &mut cert_resolver,
         pool.host,
         Path::new(certificate_path),
@@ -106,36 +103,4 @@ async fn listen_for_https_request(shared_data: Arc<SharedData>) -> Result<(), io
   let acceptor = https.produce_acceptor(LOCAL_HTTPS_ADDRESS).await?;
 
   server::create(acceptor, shared_data, true).await
-}
-
-fn add_certificate(
-  cert_resolver: &mut ResolvesServerCertUsingSNI,
-  dns_name: &str,
-  certificate_path: &Path,
-  private_key_path: &Path,
-) -> Result<(), io::Error> {
-  let certificates = load_certs(certificate_path)?;
-  let private_key = load_key(private_key_path)?;
-  let private_key = RSASigningKey::new(&private_key).map_err(|_| io::Error::new(InvalidData, "invalid rsa key"))?;
-  let certificate_key = CertifiedKey::new(certificates, Arc::new(Box::new(private_key)));
-  cert_resolver
-    .add(dns_name, certificate_key)
-    .map_err(|e| io::Error::new(InvalidData, e))
-}
-
-fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
-  let file = File::open(path)?;
-  let mut reader = BufReader::new(file);
-  certs(&mut reader).map_err(|_| io::Error::new(InvalidData, "invalid cert"))
-}
-
-fn load_key(path: &Path) -> io::Result<PrivateKey> {
-  let mut keys = load_keys(path)?;
-  Ok(keys.remove(0))
-}
-
-fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
-  let file = File::open(path)?;
-  let mut reader = BufReader::new(file);
-  rsa_private_keys(&mut reader).map_err(|_| io::Error::new(InvalidData, "invalid key"))
 }

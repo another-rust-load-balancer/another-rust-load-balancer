46547f5987177132657750a7efe93f94248a8dc7
Martinif <silas@gaf.fs.lmu.de>
2021-01-05 10:32:17 +0100

    Re-add RoundRobin, IPHash

--- src/lb_strategies.rs
+++ src/lb_strategies.rs
@@ -1,5 +1,7 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use std::net::SocketAddr;
-
+use std::sync::{Arc, Mutex};
 use rand::{thread_rng, Rng};
 
 pub trait LBStrategy: std::fmt::Debug {
@@ -7,15 +9,11 @@ pub trait LBStrategy: std::fmt::Debug {
 }
 
 #[derive(Debug)]
-pub struct RandomStrategy {
-  // TODO save rng for all calls -> rng: ThreadRng,
-}
+pub struct RandomStrategy {}
 
 impl RandomStrategy {
   pub fn new() -> RandomStrategy {
-    RandomStrategy {
-      // rng: rand::thread_rng(),
-    }
+    RandomStrategy {}
   }
 }
 
@@ -23,5 +21,43 @@ impl LBStrategy for RandomStrategy {
   fn resolve_address_index(&self, address_count: usize, _remote_addr: &SocketAddr) -> usize {
     let mut rng = thread_rng();
     rng.gen_range(0..address_count)
+  }
+}
+
+#[derive(Debug)]
+pub struct IPHashStrategy {}
+
+impl IPHashStrategy {
+  pub fn new() -> IPHashStrategy {
+    IPHashStrategy {}
+  }
+}
+
+impl LBStrategy for IPHashStrategy {
+  fn resolve_address_index(&self, address_count: usize, _remote_addr: &SocketAddr) -> usize {
+    let mut hasher = DefaultHasher::new();
+    _remote_addr.port().hash(&mut hasher);
+    (hasher.finish() % address_count as u64) as usize
+  }
+}
+
+#[derive(Debug)]
+pub struct RoundRobinStrategy {
+  rrc: Arc<Mutex<u32>>,
+}
+
+impl RoundRobinStrategy {
+  pub fn new() -> RoundRobinStrategy {
+    RoundRobinStrategy {
+      rrc : Arc::new(Mutex::new(0))
+    }
+  }
+}
+
+impl LBStrategy for RoundRobinStrategy {
+  fn resolve_address_index(&self, address_count: usize, _remote_addr: &SocketAddr) -> usize {
+    let mut rrchandle = self.rrc.lock().unwrap();
+    *rrchandle = (*rrchandle +1) % address_count as u32;
+    *rrchandle as usize
   }
 }

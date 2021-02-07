e585e1778771488f125dc9742550d36ded87e8d6
skess42 <skagaros@gmail.com>
2021-02-07 17:00:14 +0100

    Refactor authentication

--- src/configuration.rs
+++ src/configuration.rs
@@ -257,39 +257,12 @@ impl TryFrom<(String, Value)> for Box<dyn Middleware> {
 
   fn try_from((name, payload): (String, Value)) -> Result<Self, Self::Error> {
     match (name.as_str(), payload) {
-      ("Authentication", Value::Table(t)) => {
-        let ldap_address = t
-          .get("ldap_address")
-          .map_or(Err(()), |value| Ok(value))?
-          .as_str()
-          .map_or(Err(()), |v| Ok(v))?
-          .to_string();
-        let user_directory = t
-          .get("user_directory")
-          .map_or(Err(()), |value| Ok(value))?
-          .as_str()
-          .map_or(Err(()), |v| Ok(v))?
-          .to_string();
-        let rdn_identifier = t
-          .get("rdn_identifier")
-          .map_or(Err(()), |value| Ok(value))?
-          .as_str()
-          .map_or(Err(()), |v| Ok(v))?
-          .to_string();
-        let recursive = t
-          .get("recursive")
-          .map_or(Err(()), |value| Ok(value))?
-          .as_bool()
-          .map_or(Err(()), |v| Ok(v))?;
-        Ok(Box::new(Authentication {
-          ldap_address,
-          user_directory,
-          rdn_identifier,
-          recursive,
-        }))
-      }
-
-      // Ok(Box::new(Authentication {  })),
+      ("Authentication", Value::Table(t)) => Ok(Box::new(Authentication {
+        ldap_address: t.get("ldap_address").and_then(Value::as_str).ok_or(())?.to_string(),
+        user_directory: t.get("user_directory").and_then(Value::as_str).ok_or(())?.to_string(),
+        rdn_identifier: t.get("rdn_identifier").and_then(Value::as_str).ok_or(())?.to_string(),
+        recursive: t.get("recursive").and_then(Value::as_bool).ok_or(())?,
+      })),
       ("Compression", _) => Ok(Box::new(Compression)),
       ("HttpsRedirector", _) => Ok(Box::new(HttpsRedirector)),
       ("MaxBodySize", Integer(limit)) => Ok(Box::new(MaxBodySize { limit })),

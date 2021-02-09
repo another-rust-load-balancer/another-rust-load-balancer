a10611e4ac955c1b5ee8a895d4c6ef23b05d570f
Adrodoc <adrodoc55@googlemail.com>
2021-02-09 18:32:23 +0100

    Add documentation for trait Middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -13,8 +13,23 @@ pub mod https_redirector;
 pub mod maxbodysize;
 pub mod rate_limiter;
 
+/// A trait for implementing middlewares, see
+/// [`forward_request`](Middleware::forward_request) for more details.
 #[async_trait]
 pub trait Middleware: Send + Sync + std::fmt::Debug {
+  /// Forward the `request` via [`MiddlewareChain::forward_request`],
+  /// potentially modifying the request or response along the way. The request
+  /// can also be answered early (from cache or in cases like missing
+  /// authentication) in which case the supplied `chain` is not used.
+  ///
+  /// The default implementation calls [`modify_request`][], forwards the
+  /// modified request via [`MiddlewareChain::forward_request`] and then calls
+  /// [`modify_response`][] on the received response. Simple implementations
+  /// should prefer to just implement [`modify_request`][] or
+  /// [`modify_response`][] if possible.
+  ///
+  /// [`modify_request`]: Middleware::modify_request
+  /// [`modify_response`]: Middleware::modify_response
   async fn forward_request(
     &self,
     request: Request<Body>,
@@ -30,6 +45,19 @@ pub trait Middleware: Send + Sync + std::fmt::Debug {
     }
   }
 
+  /// Transforms the `request` before it is forwarded to later middlewares and
+  /// then the backend server or returns an early response (from cache or in
+  /// cases like missing authentication).
+  ///
+  /// If an early response is returned then
+  /// [`modify_response`](Middleware::modify_response) of this middleware is not
+  /// called.
+  ///
+  /// This method serves only as a convenience to implement simple middlewares.
+  /// If [`forward_request`](Middleware::forward_request) is implemented, this
+  /// function is never called.
+  ///
+  /// The default implementation just returns the original `request`.
   async fn modify_request(
     &self,
     request: Request<Body>,
@@ -38,6 +66,17 @@ pub trait Middleware: Send + Sync + std::fmt::Debug {
     Ok(request)
   }
 
+  /// Transforms the response before it is returned to the calling client.
+  ///
+  /// This function is not called for early responses of the same middleware,
+  /// but it is called for early responses of middlewares further down the
+  /// [`MiddlewareChain`].
+  ///
+  /// This method serves only as a convenience to implement simple middlewares.
+  /// If [`forward_request`](Middleware::forward_request) is implemented, this
+  /// function is never called.
+  ///
+  /// The default implementation just returns the original `response`.
   async fn modify_response(&self, response: Response<Body>, _context: &Context<'_>) -> Response<Body> {
     response
   }

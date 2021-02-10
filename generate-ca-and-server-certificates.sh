6868c10d11d25fb0510153b894468acce017aa1c
Adrodoc <adrodoc55@googlemail.com>
2021-02-10 16:40:47 +0100

    Split bash files to generate certificates

--- generate-ca-and-server-certificates.sh
+++ generate-ca-and-server-certificates.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# Create our CA and sign a server certificate as described in https://www.makethenmakeinstall.com/2014/05/ssl-client-authentication-step-by-step/ and https://security.stackexchange.com/a/176084.
-
-mkdir x509
-cd x509
-openssl req -subj "/C=DE/ST=Deutschland/L=Muenchen/O=Another Rust Load Balancer/CN=Another Rust Load Balancer CA" -newkey rsa:4096 -keyform PEM -keyout ca.key -x509 -days 3650 -outform PEM -out ca.cer -nodes
-
-openssl genrsa -out localhost.key 4096
-openssl req -new -subj "/C=DE/ST=Deutschland/L=Muenchen/O=Another Rust Load Balancer/CN=localhost" -addext "subjectAltName=DNS:localhost" -key localhost.key -out localhost.csr -sha256
-echo "[server]
-subjectAltName=DNS:localhost
-" > localhost.cnf
-openssl x509 -req -in localhost.csr -CA ca.cer -CAkey ca.key -set_serial 1 -extensions server -days 1024 -outform PEM -out localhost.cer -sha256 -extfile localhost.cnf
-cat ca.cer >> localhost.cer
-
-openssl genrsa -out www.arlb.de.key 4096
-openssl req -new -subj "/C=DE/ST=Deutschland/L=Muenchen/O=Another Rust Load Balancer/CN=www.arlb.de" -addext "subjectAltName=DNS:www.arlb.de" -key www.arlb.de.key -out www.arlb.de.csr -sha256
-echo "[server]
-subjectAltName=DNS:www.arlb.de
-" > www.arlb.de.cnf
-openssl x509 -req -in www.arlb.de.csr -CA ca.cer -CAkey ca.key -set_serial 2 -extensions server -days 1024 -outform PEM -out www.arlb.de.cer -sha256 -extfile www.arlb.de.cnf
-cat ca.cer >> www.arlb.de.cer
-
-openssl genrsa -out https.localhost.key 4096
-openssl req -new -subj "/C=DE/ST=Deutschland/L=Muenchen/O=Another Rust Load Balancer/CN=https.localhost" -addext "subjectAltName=DNS:https.localhost" -key https.localhost.key -out https.localhost.csr -sha256
-echo "[server]
-subjectAltName=DNS:https.localhost
-" > https.localhost.cnf
-openssl x509 -req -in https.localhost.csr -CA ca.cer -CAkey ca.key -set_serial 2 -extensions server -days 1024 -outform PEM -out https.localhost.cer -sha256 -extfile https.localhost.cnf
-cat ca.cer >> https.localhost.cer
+./generate-ca-certificate.sh
+./generate-server-certificate.sh "https.localhost"
+./generate-server-certificate.sh "www.arlb.de"
---
+++ generate-server-certificate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create our CA and sign a server certificate as described in https://www.makethenmakeinstall.com/2014/05/ssl-client-authentication-step-by-step/ and https://security.stackexchange.com/a/176084.
+
+domain_name=$1
+
+if [ -z "${domain_name}" ]; then
+    echo "Usage: $0 <domain_name>" 1>&2
+    exit 1
+fi
+
+cd x509
+typeset -i serial=$(cat serial.txt)+1
+echo $serial > serial.txt
+
+openssl genrsa -out $domain_name.key 4096
+openssl req -new -subj "/C=DE/ST=Deutschland/L=Muenchen/O=Another Rust Load Balancer/CN=$domain_name" -addext "subjectAltName=DNS:$domain_name" -key $domain_name.key -out $domain_name.csr -sha256
+echo "[server]
+subjectAltName=DNS:$domain_name
+" > $domain_name.cnf
+openssl x509 -req -in $domain_name.csr -CA ca.cer -CAkey ca.key -set_serial $serial -extensions server -days 1024 -outform PEM -out $domain_name.cer -sha256 -extfile $domain_name.cnf
+cat ca.cer >> $domain_name.cer
